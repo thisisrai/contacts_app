36e65b9b75a1a48fa5f2fd050bb0b9626c59d89b
Rai Lee <thisisrailee@gmail.com>
2017-05-15 11:39:11 -0700

    Debugged: named app with appropriate name, installed necessary depedencies, and created a contacts db. App runs on server.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const database = require('./database')
+const database = require('./database.js')
 const app = express()
 
 require('ejs')
